230b22b3b27eba79aff33553a916a62dcb297f65
SnowKagamine <a159753943@yahoo.com.tw>
2019-10-10 07:07:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
+const token = require("./config.json");
+bot.on('ready',()=> {
+    console.log(`Bot logging at discord ${bot.user.tag}!`);
+});
+
+bot.on('message' , msg => {
+    if(msg.content ==='ping'){
+        msg.reply("Pong!");
+
+
+    }
+});
+
+bot.login(config.token)
