e989a57d0292ce19d5fe77c4ae6b59233c8d4b7c
SnowKagamine <a159753943@yahoo.com.tw>
2019-10-10 07:59:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
 const token = require("./config.json");
-bot.login(config.token)
+
 bot.on('ready',()=> {
     console.log(`Bot logging at discord ${bot.user.tag}!`);
 });
