a6e36e350ed29a86f4a933b181a92340b68a0d35
SnowKagamine <a159753943@yahoo.com.tw>
2019-10-10 08:03:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-var token='bot.login(config.token)';
+var token='NjMxNjIwNTI0OTc5NTE5NDk4.XZ5wDA.HVjF7rj1a5kiV_mTHISRUezQgFs';
 bot.on('ready',()=> {
     console.log(`Bot logging at discord ${bot.user.tag}!`);
 });
@@ -13,4 +13,4 @@ bot.on('message' , msg => {
     }
 });
 
-bot.login(token)
+bot.login(token);
